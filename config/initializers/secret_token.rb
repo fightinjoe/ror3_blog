1eb0757c1f49fece588b84e6735bd05c38398638
Aaron Wheeler <aaron@fightinjoe.com>
2010-09-20 12:22:38 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fightinjoe::Application.config.secret_token = '1f40a24f38e083f8cdc721dfdee0e068fa8ad90c972a45d238a4e33f339f5ce6f55d266e0d42fff281c6947502588ca4012fa0fcebf5fc5e7eb70f7ba8cd7bd5'
